257eb8fabba63d7112b843b500f4b49a4a549f68
suleman81 <sunnybajwa904@gmail.com>
2023-01-27 12:42:51 +0500

    commit

--- app/src/main/java/com/moutamid/hbdresidents/fragments/MyAccountFragment.java
+++ app/src/main/java/com/moutamid/hbdresidents/fragments/MyAccountFragment.java
@@ -1,5 +1,6 @@
 package com.moutamid.hbdresidents.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,8 +10,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.moutamid.hbdresidents.R;
+import com.moutamid.hbdresidents.databinding.FragmentMyAccountBinding;
 
 public class MyAccountFragment extends Fragment {
+    FragmentMyAccountBinding binding;
+    Context context;
 
     public MyAccountFragment() {
         // Required empty public constructor
@@ -18,7 +22,12 @@ public class MyAccountFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentMyAccountBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        context = view.getContext();
 
-        return inflater.inflate(R.layout.fragment_my_account, container, false);
+
+
+        return view;
     }
 }
