755d73dfa5f76ed4f2fe9fb4a32224b7c9485d0c
suleman81 <sunnybajwa904@gmail.com>
2023-01-27 17:33:33 +0500

    commit

--- app/src/main/java/com/moutamid/hbdresidents/fragments/MyAccountFragment.java
+++ app/src/main/java/com/moutamid/hbdresidents/fragments/MyAccountFragment.java
@@ -1,6 +1,7 @@
 package com.moutamid.hbdresidents.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,12 +10,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+import com.moutamid.hbdresidents.EditProfileActivity;
 import com.moutamid.hbdresidents.R;
+import com.moutamid.hbdresidents.SplashScreenActivity;
 import com.moutamid.hbdresidents.databinding.FragmentMyAccountBinding;
 import com.moutamid.hbdresidents.models.UserModel;
 import com.moutamid.hbdresidents.utilis.Constants;
@@ -32,6 +36,20 @@ public class MyAccountFragment extends Fragment {
         binding = FragmentMyAccountBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
         context = view.getContext();
+
+        binding.editProfile.setOnClickListener(v -> {
+            startActivity(new Intent(context, EditProfileActivity.class));
+        });
+
+        binding.logout.setOnClickListener(v->{
+            Constants.auth().signOut();
+            startActivity(new Intent(context, SplashScreenActivity.class));
+            getActivity().finish();
+        });
+
+        binding.delete.setOnClickListener(v -> {
+            Toast.makeText(context, "OK COOL", Toast.LENGTH_SHORT).show();
+        });
 
         Constants.databaseReference().child("users").child(Constants.auth().getCurrentUser().getUid())
                 .addValueEventListener(new ValueEventListener() {
