1061685327c53012e80c0185c4ff7e3d3f9c42ff
suleman81 <sunnybajwa904@gmail.com>
2023-01-27 15:21:48 +0500

    commit

--- app/src/main/java/com/moutamid/hbdresidents/fragments/HomeFragment.java
+++ app/src/main/java/com/moutamid/hbdresidents/fragments/HomeFragment.java
@@ -3,18 +3,26 @@ package com.moutamid.hbdresidents.fragments;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import com.moutamid.hbdresidents.R;
 import com.moutamid.hbdresidents.databinding.FragmentHomeBinding;
+import com.moutamid.hbdresidents.models.UserModel;
+import com.moutamid.hbdresidents.utilis.Constants;
 
 import java.util.ArrayList;
 
@@ -47,6 +55,20 @@ public class HomeFragment extends Fragment {
         binding.dengueChart.setDescription(denDes);
         binding.dengueChart.animateXY(2000, 2000);
         binding.dengueChart.invalidate();
+
+        Constants.databaseReference().child("users").child(Constants.auth().getCurrentUser().getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        UserModel model = snapshot.getValue(UserModel.class);
+                        Glide.with(context).load(model.getImage()).placeholder(R.drawable.profile_icon).into(binding.profile);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
 
 
         return view;
--- app/src/main/java/com/moutamid/hbdresidents/fragments/MyAccountFragment.java
+++ app/src/main/java/com/moutamid/hbdresidents/fragments/MyAccountFragment.java
@@ -3,14 +3,21 @@ package com.moutamid.hbdresidents.fragments;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 import com.moutamid.hbdresidents.R;
 import com.moutamid.hbdresidents.databinding.FragmentMyAccountBinding;
+import com.moutamid.hbdresidents.models.UserModel;
+import com.moutamid.hbdresidents.utilis.Constants;
 
 public class MyAccountFragment extends Fragment {
     FragmentMyAccountBinding binding;
@@ -26,6 +33,20 @@ public class MyAccountFragment extends Fragment {
         View view = binding.getRoot();
         context = view.getContext();
 
+        Constants.databaseReference().child("users").child(Constants.auth().getCurrentUser().getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        UserModel model = snapshot.getValue(UserModel.class);
+                        Glide.with(context).load(model.getImage()).placeholder(R.drawable.profile_icon).into(binding.profileImage);
+                        binding.name.setText(model.getName());
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
 
 
         return view;
