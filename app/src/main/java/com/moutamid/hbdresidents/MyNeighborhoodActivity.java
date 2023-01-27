76c4c5f16875ee6db2230702d22d45640b8b748c
suleman81 <sunnybajwa904@gmail.com>
2023-01-27 11:14:32 +0500

    commit

--- app/src/main/java/com/moutamid/hbdresidents/MyNeighborhoodActivity.java
+++ app/src/main/java/com/moutamid/hbdresidents/MyNeighborhoodActivity.java
@@ -1,14 +1,47 @@
 package com.moutamid.hbdresidents;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MyNeighborhoodActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.moutamid.hbdresidents.databinding.ActivityMyNeighborhoodBinding;
+import com.moutamid.hbdresidents.fragments.HomeFragment;
+import com.moutamid.hbdresidents.fragments.MyAccountFragment;
+import com.moutamid.hbdresidents.fragments.ReachFragment;
 
+public class MyNeighborhoodActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    ActivityMyNeighborhoodBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_neighborhood);
+        binding = ActivityMyNeighborhoodBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNav.setOnNavigationItemSelectedListener(this);
+        binding.bottomNav.setSelectedItemId(R.id.actionHome);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.actionHome:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout , new HomeFragment()).commit();
+                return true;
+
+            case R.id.actionAccount:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, new MyAccountFragment()).commit();
+                return true;
+
+            case R.id.actionReach:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, new ReachFragment()).commit();
+                return true;
+
+        }
+        return false;
     }
 }
--- app/src/main/java/com/moutamid/hbdresidents/fragments/HomeFragment.java
+++ app/src/main/java/com/moutamid/hbdresidents/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.moutamid.hbdresidents.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,59 +9,102 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.moutamid.hbdresidents.R;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.moutamid.hbdresidents.databinding.FragmentHomeBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FragmentHomeBinding binding;
+    Context context;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        context = view.getContext();
+
+        Description psiDes = new Description();
+        psiDes.setText("PSI LEVEL CHART");
+        Description denDes = new Description();
+        denDes.setText("DENGUE LEVEL CHART");
+
+        BarData data = new BarData(getDataSet());
+        binding.psiChart.setData(data);
+        binding.psiChart.setDescription(psiDes);
+        binding.psiChart.animateXY(2000, 2000);
+        binding.psiChart.invalidate();
+
+        binding.dengueChart.setData(data);
+        binding.dengueChart.setDescription(denDes);
+        binding.dengueChart.animateXY(2000, 2000);
+        binding.dengueChart.invalidate();
+
+
+        return view;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    private ArrayList<IBarDataSet> getDataSet() {
+        ArrayList<IBarDataSet> dataSets = null;
+
+        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
+        BarEntry v1e1 = new BarEntry(110.000f, 0); // Jan
+        valueSet1.add(v1e1);
+        BarEntry v1e2 = new BarEntry(40.000f, 1); // Feb
+        valueSet1.add(v1e2);
+        BarEntry v1e3 = new BarEntry(60.000f, 2); // Mar
+        valueSet1.add(v1e3);
+        BarEntry v1e4 = new BarEntry(30.000f, 3); // Apr
+        valueSet1.add(v1e4);
+        BarEntry v1e5 = new BarEntry(90.000f, 4); // May
+        valueSet1.add(v1e5);
+        BarEntry v1e6 = new BarEntry(100.000f, 5); // Jun
+        valueSet1.add(v1e6);
+        BarEntry v1e7 = new BarEntry(1000.000f, 6); // Jun
+        valueSet1.add(v1e7);
+        BarEntry v1e8 = new BarEntry(10.000f, 7); // Jun
+        valueSet1.add(v1e8);
+        BarEntry v1e9 = new BarEntry(500.000f, 8); // Jun
+        valueSet1.add(v1e9);
+        BarEntry v1e10 = new BarEntry(100.000f, 9); // Jun
+        valueSet1.add(v1e10);
+        BarEntry v1e11 = new BarEntry(1.000f, 10); // Jun
+        valueSet1.add(v1e11);
+        BarEntry v1e12 = new BarEntry(100.000f, 11); // Jun
+        valueSet1.add(v1e12);
+
+        BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Level");
+        //barDataSet1.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        dataSets = new ArrayList<com.github.mikephil.charting.interfaces.datasets.IBarDataSet>();
+        dataSets.add(barDataSet1);
+        dataSets.add(barDataSet1);
+        return dataSets;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    private ArrayList<String> getXAxisValues() {
+        ArrayList<String> xAxis = new ArrayList<String>();
+        xAxis.add("JAN");
+        xAxis.add("FEB");
+        xAxis.add("MAR");
+        xAxis.add("APR");
+        xAxis.add("MAY");
+        xAxis.add("JUN");
+        xAxis.add("JUL");
+        xAxis.add("AUG");
+        xAxis.add("SEP");
+        xAxis.add("OCT");
+        xAxis.add("NOV");
+        xAxis.add("DEC");
+        return xAxis;
     }
+
 }
