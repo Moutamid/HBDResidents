1d0a6560b19e5c71ad73dd4bd86dcf7cce0ec259
suleman81 <sunnybajwa904@gmail.com>
2023-01-28 15:26:48 +0500

    commit

--- app/src/main/java/com/moutamid/hbdresidents/fragments/ReachFragment.java
+++ app/src/main/java/com/moutamid/hbdresidents/fragments/ReachFragment.java
@@ -1,5 +1,6 @@
 package com.moutamid.hbdresidents.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,58 +10,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.moutamid.hbdresidents.R;
+import com.moutamid.hbdresidents.databinding.FragmentReachBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ReachFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ReachFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FragmentReachBinding binding;
+    Context context;
 
     public ReachFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ReachFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ReachFragment newInstance(String param1, String param2) {
-        ReachFragment fragment = new ReachFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentReachBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        context = view.getContext();
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reach, container, false);
+        return view;
     }
 }
