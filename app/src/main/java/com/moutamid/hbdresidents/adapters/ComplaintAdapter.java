dbb2c0ca18c7027d04c97dc827fe9d84f4ea0097
suleman81 <sunnybajwa904@gmail.com>
2023-01-30 18:07:38 +0500

    commit

---
+++ app/src/main/java/com/moutamid/hbdresidents/adapters/ComplaintAdapter.java
@@ -0,0 +1,49 @@
+package com.moutamid.hbdresidents.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.moutamid.hbdresidents.R;
+import com.moutamid.hbdresidents.models.ComplaintModel;
+
+import java.util.ArrayList;
+
+public class ComplaintAdapter extends RecyclerView.Adapter<ComplaintAdapter.ComplaintVH> {
+    Context context;
+    ArrayList<ComplaintModel> list;
+
+    public ComplaintAdapter(Context context, ArrayList<ComplaintModel> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ComplaintVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // TODO: change Layout
+        View view = LayoutInflater.from(context).inflate(R.layout.complaint_card, parent, false);
+        return new ComplaintVH(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ComplaintVH holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ComplaintVH extends RecyclerView.ViewHolder{
+
+        public ComplaintVH(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
