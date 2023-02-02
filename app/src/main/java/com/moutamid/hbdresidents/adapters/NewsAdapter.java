40509ba5e89061d1849b098c15e9c92f0189fdb6
suleman81 <sunnybajwa904@gmail.com>
2023-02-02 11:50:40 +0500

    commit

---
+++ app/src/main/java/com/moutamid/hbdresidents/adapters/NewsAdapter.java
@@ -0,0 +1,75 @@
+package com.moutamid.hbdresidents.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.moutamid.hbdresidents.R;
+import com.moutamid.hbdresidents.models.NewsModel;
+
+import org.w3c.dom.Text;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsVH> {
+    Context context;
+    ArrayList<NewsModel> list;
+
+    public NewsAdapter(Context context, ArrayList<NewsModel> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public NewsVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.news_letter, parent, false);
+        return new NewsVH(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NewsVH holder, int position) {
+        NewsModel newsModel = list.get(position);
+        holder.title.setText(newsModel.getName());
+        holder.desc.setText(newsModel.getDescription());
+        Glide.with(context).load(newsModel.getUrlToImage()).into(holder.newsImage);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Instant instant = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            instant = Instant.parse( newsModel.getPublishedAt() );
+            Date date = Date.from(instant);
+            String d = dateFormat.format(date);
+            holder.date.setText(d);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class NewsVH extends RecyclerView.ViewHolder {
+        ImageView newsImage;
+        TextView title, desc, date;
+        public NewsVH(@NonNull View itemView) {
+            super(itemView);
+            newsImage = itemView.findViewById(R.id.newsImage);
+            title = itemView.findViewById(R.id.title);
+            desc = itemView.findViewById(R.id.desc);
+            date = itemView.findViewById(R.id.date);
+        }
+    }
+
+}
--- app/src/main/java/com/moutamid/hbdresidents/fragments/HomeFragment.java
+++ app/src/main/java/com/moutamid/hbdresidents/fragments/HomeFragment.java
@@ -5,11 +5,19 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.bumptech.glide.Glide;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.BarData;
@@ -20,15 +28,25 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 import com.moutamid.hbdresidents.R;
+import com.moutamid.hbdresidents.adapters.NewsAdapter;
 import com.moutamid.hbdresidents.databinding.FragmentHomeBinding;
+import com.moutamid.hbdresidents.models.NewsModel;
 import com.moutamid.hbdresidents.models.UserModel;
 import com.moutamid.hbdresidents.utilis.Constants;
+import com.moutamid.hbdresidents.utilis.VolleySingleton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
     FragmentHomeBinding binding;
     Context context;
+    RequestQueue requestQueue;
+    ArrayList<NewsModel> newsList;
+    NewsAdapter adapter;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -39,6 +57,13 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
         context = view.getContext();
+
+        newsList = new ArrayList<>();
+
+        requestQueue = VolleySingleton.getmInstance(context).getRequestQueue();
+
+        binding.newsRC.setLayoutManager(new LinearLayoutManager(requireContext()));
+        binding.newsRC.setHasFixedSize(false);
 
         Constants.databaseReference().child("users").child(Constants.auth().getCurrentUser().getUid())
                 .addValueEventListener(new ValueEventListener() {
@@ -56,16 +81,53 @@ public class HomeFragment extends Fragment {
 
         showBarChart();
 
+        getNews();
+
         return view;
     }
 
-    private void showBarChart(){
+    private void getNews() {
+//        String url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d9ab5f282fec44f8b6d351833b2706b0";
+        String url = "https://saurav.tech/NewsAPI/top-headlines/category/health/us.json";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, response -> {
+            try {
+                JSONArray jsonArray = response.getJSONArray("articles");
+                for (int i=0; i<jsonArray.length(); i++){
+                    JSONObject object = jsonArray.getJSONObject(i);
+                    JSONObject source = object.getJSONObject("source");
+                    NewsModel model = new NewsModel();
+                    model.setId(source.getString("id"));
+                    model.setName(source.getString("name"));
+                    model.setAuthor(object.getString("author"));
+                    model.setTitle(object.getString("title"));
+                    model.setDescription(object.getString("description"));
+                    model.setUrl(object.getString("url"));
+                    model.setUrlToImage(object.getString("urlToImage"));
+                    model.setPublishedAt(object.getString("publishedAt"));
+                    model.setContent(object.getString("content"));
+                    newsList.add(model);
+                }
+                adapter = new NewsAdapter(context, newsList);
+                binding.newsRC.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+            } catch (JSONException e) {
+                Toast.makeText(context, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }, error -> {
+            Log.d("Testing123", ""+error.getLocalizedMessage());
+            throw new RuntimeException(error);
+        });
+
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    private void showBarChart() {
         ArrayList<Double> valueList = new ArrayList<Double>();
         ArrayList<BarEntry> entries = new ArrayList<>();
         String title = "Title";
 
         //input data
-        for(int i = 0; i < 6; i++){
+        for (int i = 0; i < 6; i++) {
             valueList.add(i * 100.1);
         }
 
